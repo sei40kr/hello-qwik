3781a57d0a4ede8578872e9e626affe2c21643ee
Seong Yong-ju <yongju.seong@codetakt.com>
2024-03-17 23:05:30 +0900

    tutorial: Fetching resource on state change

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useResource$, useStore } from "@builder.io/qwik";
 
 export default component$(() => {
 	const github = useStore({
 		org: "BuilderIO",
-		repos: ["qwik", "partytown"] as string[] | null,
 	});
 
+	const reposResource = useResource$<string[]>(({ track, cleanup }) => {
+		track(() => github.org);
+
+		const controller = new AbortController();
+		cleanup(() => controller.abort());
+
+		return getRepositories(github.org, controller);
+	});
+
+	console.log("Render");
 	return (
 		<main>
 			<p>
@@ -20,20 +29,22 @@ export default component$(() => {
 				</label>
 			</p>
 			<section>
-				{github.repos ? (
-					<ul>
-						{github.repos.map((repo) => (
-							<li>
-								<a href={`https://github.com/${github.org}/${repo}`}>
-									{github.org}/{repo}
-								</a>
-							</li>
-						))}
-					</ul>
-				) : (
-					"loading..."
-				)}
 			</section>
 		</main>
 	);
 });
+
+export async function getRepositories(
+	username: string,
+	controller?: AbortController,
+): Promise<string[]> {
+	console.log("FETCH", `https://api.github.com/users/${username}/repos`);
+	const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
+		signal: controller?.signal,
+	});
+	console.log("FETCH resolved");
+	const json = await resp.json();
+	return Array.isArray(json)
+		? json.map((repo: { name: string }) => repo.name)
+		: [];
+}
